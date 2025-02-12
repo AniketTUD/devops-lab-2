ac31b20937d418470b70b142c5cd2c0f8fd02eae
Patricksowon-6 <patrickakins03@gmail.com>
2025-02-12 13:32:15 +0000

    Made AVG PY Code

--- lab2.py
+++ lab2.py
@@ -1,2 +1,15 @@
-print("This is another String!")
-print("Welcome!")
+NUMBERS = 6
+
+nums = [5, 2, 6, 64, 15, 10]
+
+largest = nums[0]
+smallest = nums[0]
+
+for num in nums:
+    if largest < num:
+        largest = num
+    if smallest > num:
+        smallest = num
+
+print(f"Largest Number: {largest}")
+print(f"Smallest Number: {smallest}")
