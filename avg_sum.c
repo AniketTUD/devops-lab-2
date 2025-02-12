f4309a2db4116647ff729e076b00db21255a1e06
Patricksowon-6 <patrickakins03@gmail.com>
2025-02-12 13:49:23 +0000

    Changed avg.c Code

--- avg_sum.c
+++ avg_sum.c
@@ -1,13 +1,26 @@
-#include <stdio.h>
+#include<stdio.h>
+
+#define NUMBERS 6
 
 int main()
-
 {
-    int num1 = 4;
-    int num2 = 10;
-    int num3 = 7;
+    int nums[NUMBERS] = {5, 2, 6, 64, 15, 10};
 
-    int average = (num1 + num2 + num3) / 3;
+    int largest = nums[0];
+    int smallest = nums[0];
 
-    printf("Average is: %d", average);
+    for (int i = 0; i < NUMBERS; i++)
+    {
+        if (largest < nums[i])
+        {
+            largest = nums[i];
+        }
+        if (smallest > nums[i])
+        {
+            smallest = nums[i];
+        }
+    }
+
+    printf("Largest Number: %d\n", largest);
+    printf("Smallest Number: %d\n", smallest);
 }
